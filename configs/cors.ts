048af083527bf3187234649e1afbd9ff88acefaa
Heesu Kim <nikolaikim27@gmail.com>
2025-12-05 23:32:31 +0900

    fix config error

--- configs/cors.ts
+++ configs/cors.ts
@@ -1,9 +1,16 @@
 export const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:8080",
-    "https://coym.netlify.app"
-  ],
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:3000",
+      "http://localhost:8080",
+      "https://coym.netlify.app"
+    ];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   optionSuccessStatus: 200
--- server.ts
+++ server.ts
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors(corsOptions))
+app.options("*", cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(accountRouter)
