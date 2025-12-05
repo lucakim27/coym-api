ee96954549a806e32dfdd98313894f508a42c34d
Heesu Kim <nikolaikim27@gmail.com>
2025-12-05 23:21:01 +0900

    Update db.ts

--- configs/db.ts
+++ configs/db.ts
@@ -6,6 +6,7 @@ export const pool = mysql2.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 3306,
     enableKeepAlive: true,
     waitForConnections: true
 })
