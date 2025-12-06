2490e47b6bcdeb49baa3fd79b4c10c979b7e03e4
Heesu Kim <nikolaikim27@gmail.com>
2025-12-06 12:46:46 +0900

    remove cors

--- configs/cors.ts
+++ configs/cors.ts
@@ -1,16 +1,13 @@
-import express, { Request, Response } from "express";
-import cors, { CorsOptions } from "cors";
+import { CorsOptions } from "cors";
 
-const app = express();
-
-const corsOptions: CorsOptions = {
-  origin: (origin, callback) => {
+export const corsOptions: CorsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
     const allowedOrigins = [
       "http://localhost:3000",
       "http://localhost:8080",
       "https://coym.netlify.app"
     ];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
@@ -20,9 +17,3 @@ const corsOptions: CorsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   optionsSuccessStatus: 200
 };
-
-app.use(cors(corsOptions));
-
-app.options("*", (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
--- server.ts
+++ server.ts
@@ -1,32 +1,33 @@
-import http from 'http'
-import cors from "cors"
-import express from 'express'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import accountRouter from './routes/accountRoute'
-import commentRouter from './routes/commentRoute'
-import likeRouter from './routes/likeRoute'
-import majorRouter from './routes/majorRoute'
-import replyRouter from './routes/replyRoute'
-import requestRouter from './routes/requestRoute'
-import testRouter from './routes/testRoute'
-import { corsOptions } from './configs/cors'
+import http from "http";
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import { corsOptions } from "./configs/cors";
 
-const app = express()
-const httpServer = http.createServer(app)
-const PORT = process.env.PORT || 3000
+import accountRouter from "./routes/accountRoute";
+import commentRouter from "./routes/commentRoute";
+import likeRouter from "./routes/likeRoute";
+import majorRouter from "./routes/majorRoute";
+import replyRouter from "./routes/replyRoute";
+import requestRouter from "./routes/requestRoute";
+import testRouter from "./routes/testRoute";
 
-app.use(express.json())
-app.use(cors(corsOptions))
-app.options("*", cors(corsOptions));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(accountRouter)
-app.use(commentRouter)
-app.use(likeRouter)
-app.use(majorRouter)
-app.use(replyRouter)
-app.use(requestRouter)
-app.use(testRouter)
+const app = express();
+const httpServer = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 
-httpServer.listen(PORT, () => console.log(`Http Server running on port ${PORT}.`))
+// app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(accountRouter);
+app.use(commentRouter);
+app.use(likeRouter);
+app.use(majorRouter);
+app.use(replyRouter);
+app.use(requestRouter);
+app.use(testRouter);
+
+httpServer.listen(PORT, () => console.log(`Http Server running on port ${PORT}.`));
