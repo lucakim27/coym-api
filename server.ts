41071cdb54141f3e019781a04d947d05f65724b7
Heesu Kim <nikolaikim27@gmail.com>
2025-12-06 13:43:48 +0900

    fix cors

--- server.ts
+++ server.ts
@@ -15,9 +15,24 @@ import testRouter from "./routes/testRoute";
 
 const app = express();
 const httpServer = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+const allowedOrigins = [
+  'http://localhost:8888',       // local dev
+  'https://coym.netlify.app',   // deployed site
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // allow requests with no origin (like Postman) or in allowedOrigins
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 // app.options("*", cors(corsOptions)); // pre-flight for all routes
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
